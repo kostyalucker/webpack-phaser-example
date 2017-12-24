d3b20dba7842196a3de7f42c42283729112af215
rob3n <roben1006@gmail.com>
2017-12-24 23:08:37 +0300

    add branch game

--- src/index.js
+++ src/index.js
@@ -9,12 +9,69 @@ function preload() {
     game.load.image('logo', './assets/images/phaser.png');
 }
 
+var upK, downK, leftK, rightK, man;
+
 function create() {
-    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
+     man = game.add.graphics(0, 0);
+
+    //Fon
+
+    game.stage.backgroundColor = '#999';
+
+    // graphics.lineStyle(2, 0xffd900, 1);
+
+    man.beginFill(0xFFFA62, .9);
+    man.drawRect(200,200,50,50);
+
+
+    upK = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    downK = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    leftK = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    rightK = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+
+    man
+
+    game.physics.enable(man, Phaser.Physics.ARCADE)
+
 };
 
 function update() {
     // ¯ \_(ツ)_/¯
     // "surprise me"
+
+    // Mouse
+    if (game.input.mousePointer.isDown)
+    {
+        //  400 is the speed it will move towards the mouse
+        game.physics.arcade.moveToPointer(man, 400);
+
+        //  if it's overlapping the mouse, don't move any more
+        if (Phaser.Rectangle.contains(man.body, game.input.x, game.input.y))
+        {
+            man.body.velocity.setTo(0, 0);
+        }
+    }
+    else
+    {
+        man.body.velocity.setTo(0, 0);
+    }
+
+    //Speed
+
+    var velocity = 5;
+
+    // KeyHandle
+
+    if(upK.isDown) {
+        man.y = man.y-- - velocity;
+    }
+    if(downK.isDown) {
+        man.y = man.y++ + velocity;
+    }
+    if(rightK.isDown) {
+        man.x = man.x++ + velocity;
+    }
+    if(leftK.isDown) {
+        man.x = man.x-- - velocity;
+    }
 }
